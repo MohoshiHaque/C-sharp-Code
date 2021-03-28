4eda1c10fc1b6491da0a51d6e628fb3992ab7893
Mohoshi Haque <46513655+MohoshiHaque@users.noreply.github.com>
2021-03-28 16:05:57 +0600

    Add files via upload

---
+++ Polymorphism.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polyformisim
+{
+    class myclass
+    {
+        private int _age = 30;
+
+        public virtual int age
+        {
+            set
+            {
+                this.age = age;
+            }
+            get
+            {
+                return _age;
+            }
+        }
+    }
+
+    class myclass2:myclass
+    {
+        private  int _age = 35;
+        public override int age
+        {
+            set
+            {
+                this.age = age;
+            }
+            get
+            {
+                return _age;
+            }
+        }
+
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            myclass data = new myclass2();
+            Console.WriteLine(data.age);
+            Console.ReadLine();
+
+        }
+    }
+}
