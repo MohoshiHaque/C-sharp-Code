e0aea7ae0f6876cebdddd716b51023718ab2aefb
Mohoshi Haque <46513655+MohoshiHaque@users.noreply.github.com>
2021-03-28 16:03:41 +0600

    Add files via upload

---
+++ Inheritence1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritence
+{
+
+    class vehicle
+    {
+        public string brand = "Ford";
+        public void honk()
+        {
+            Console.WriteLine("Tuut");
+        }
+    }
+
+    class Car: vehicle
+    {
+        public string model = "Mustang";
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Car myCar = new Car();
+            myCar.honk();
+            Console.WriteLine(myCar.brand + " " + myCar.model);
+
+            Console.ReadLine();
+        }
+    }
+}
