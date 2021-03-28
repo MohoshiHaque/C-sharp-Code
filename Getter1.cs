8873ab5e38954882be76f4a0e8649dc0ba839464
Mohoshi Haque <46513655+MohoshiHaque@users.noreply.github.com>
2021-03-28 16:03:11 +0600

    Add files via upload

---
+++ Getter1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Getter
+{
+
+    class Income
+    {
+
+        private int _StdId;
+        private string _Name;
+        private string _FName;
+
+
+
+        // StdId property করছি
+        public int StdId
+        {
+            set
+            {
+                if (value <= 0)
+                {
+                    Console.WriteLine("Id can't be negative or zero");
+                }
+                else
+                {
+                    this._StdId = value;
+                }
+                
+            }
+
+            get
+            {
+                return this._StdId;
+            }
+
+        }
+
+       
+       // For Name property
+        public string Name
+        {
+            set
+            {
+                this._Name = value;
+            }
+
+            get
+            {
+                return this._Name;
+
+            }
+        }
+
+
+        
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Income x = new Income();
+            x.StdId = -2;
+            x.Name = "Mohoshi";
+            Console.WriteLine(x.StdId);
+            Console.WriteLine(x.Name);
+            Console.ReadLine();
+        }
+    }
+}
