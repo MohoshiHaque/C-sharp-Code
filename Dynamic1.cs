3d8fb003761ec41aa0bf9a75a80da07252987e39
Mohoshi Haque <46513655+MohoshiHaque@users.noreply.github.com>
2021-03-28 16:01:20 +0600

    Add files via upload

---
+++ Dynamic1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dynamic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            dynamic dynamicVariable = 12;
+            Console.WriteLine(dynamicVariable + "Type is: " + dynamicVariable.GetType());
+            dynamicVariable = "Bangladesh";
+            Console.WriteLine(dynamicVariable + "Type is: " + dynamicVariable.GetType());
+            
+            print("12", 12);
+            Console.ReadLine();
+        }
+
+        public static void print(dynamic x, dynamic y)
+        {
+            Console.WriteLine(x + "-" + y);
+        }
+    }
+}
