71e411d9cb5b336359e5d4a9d1bc34c6c22555a1
Mohoshi Haque <46513655+MohoshiHaque@users.noreply.github.com>
2021-03-28 16:05:26 +0600

    Add files via upload

---
+++ Nullable.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nullable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string x = null;
+            int? a = 12;
+            int? c = null;
+            Console.WriteLine(c);
+
+            int? b = a ?? 20;
+            Console.WriteLine(b);
+            Console.ReadLine();
+        }
+    }
+}
